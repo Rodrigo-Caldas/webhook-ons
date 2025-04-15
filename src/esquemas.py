428a920004bc4649bcb42da680149246e0ad6e16
Rodrigo-Caldas <rodrigocaldas@id.uff.br>
2025-04-15 15:46:09 -0300

    :scroll: Docstring do typeddict

--- src/esquemas.py
+++ src/esquemas.py
@@ -1,8 +1,34 @@
 """Esquema de requisição do webhook."""
 
-from pydantic import BaseModel
+from typing import TypedDict
 
-class SintegrePayload(BaseModel):
+class SintegrePayload(TypedDict):
+    """
+    Estrutura do payload dos dados do Webhook.
+
+    Parameters
+    ----------
+    DataProduto: str
+        A data de referência do produto.
+
+    MacroProcesso: str
+        O nome do macroprocesso do produto.
+
+    Nome: str
+        O nome do produto.
+
+    Periodicidade: str
+        A data inicial de referência do produto.
+
+    PeriodicidadeFinal: str
+        A data final de referência do produto.
+
+    Processo: str
+        Nome do processo do produto.
+        
+    Url: str
+        Url para dowload do arquivo.
+    """
     DataProduto: str
     MacroProcesso: str
     Nome: str
