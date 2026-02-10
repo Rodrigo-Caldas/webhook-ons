e548510bad552c7e5100ac20494305f0bdfcb7b4
Rodrigo-Caldas <rodrigocaldas@id.uff.br>
2026-02-09 23:39:26 -0300

    :hammer: esquema de dados

---
+++ src/schemas/schemas.py
@@ -0,0 +1,84 @@
+"""Modelos e tipos gerais do Webhook."""
+
+from datetime import datetime
+from typing import TypedDict
+
+
+class RequestWebhook(TypedDict):
+    """
+    Estrutura bruta dos dados vindos do Webhook.
+
+    Attributes
+    ----------
+    product_date: str
+        Data do produto.
+    macro_process: str
+        Macro processo.
+    name: str
+        Nome do arquivo.
+    periodicity: str
+        Periodicidade do arquivo.
+    final_periodicity: str
+        Periodicidade final.
+    received_time: datetime
+        Data/hora de quando o arquivo foi recebido.
+    process: str
+        Processo que ele pertence
+    url: str
+        Url de download.
+    """
+
+    product_date: str
+    macro_process: str
+    name: str
+    periodicity: str
+    final_periodicity: str
+    received_time: datetime
+    process: str
+    url: str
+
+
+class WebhookDB(TypedDict):
+    """
+    Estrutura dos dados Webhook para serem inseridos no BD.
+
+    Attributes
+    ----------
+    product_date: datetime
+        Data do produto.
+    macro_process: str
+        Macro processo.
+    name: str
+        Nome do arquivo vindo do webhook.
+    std_name: str
+        Nome do arquivo vindo do webhook normalizado.
+    download_name: str
+        Nome do arquivo quando é baixado.
+    extension: str
+        Extensão do arquivo.
+    s3_path: Path
+        Caminho do arquivo salvo no S3.
+    periodicity: datetime
+        Periodicidade do arquivo.
+    final_periodicity: datetime
+        Periodicidade final.
+    received_time: datetime
+        Data/hora de quando o arquivo foi recebido.
+    process: str
+        Processo que ele pertence
+    url: str
+        Url de download.
+    """
+
+    product_date: datetime
+    macro_process: str
+    name: str
+    std_name: str
+    download_name: str
+    extension: str
+    s3_path: str
+    periodicity: datetime
+    final_periodicity: datetime
+    received_time: datetime
+    process: str
+    url: str
